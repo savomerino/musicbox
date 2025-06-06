7d8f61f4e9a51d051ab80f208d2ba2fed2f240f5
savomerino <savomerino@gmail.com>
2025-06-05 23:19:49 -0300

    Modigficado para subir

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ function App() {
     <div className={styles.app}>
       <Navbar />
       <main>
-        {/* Renderizamos 4 listados para cumplir el requerimiento  */}
+        {}
         <AlbumList title="Lanzamientos Populares" albums={mockAlbums} />
         <AlbumList title="Tus Mixes Más Escuchados" albums={mockAlbums.slice(0, 5).reverse()} />
-        <AlbumList title="Similar a Lo-Fi Chillers" albums={mockAlbums.slice(2, 7)} />
+        <AlbumList title="Similares" albums={mockAlbums.slice(2, 7)} />
         <AlbumList title="Clásicos del Rock" albums={mockAlbums.slice(4)} />
       </main>
     </div>
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,7 +3,7 @@ import styles from './Navbar.module.css';
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-      <h1 className={styles.brand}>Music Gallery</h1>
+      <h1 className={styles.brand}>MusicBox</h1>
       <div>
         <a href="#" className={styles.navLink}>Inicio</a>
         <a href="#" className={styles.navLink}>Buscar</a>
